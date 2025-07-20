7cd164b7c62207280da012fcc1f9c4f09994fe74
mohan-bee <mohn08052006@gmail.com>
2025-07-20 13:46:28 +0530

    fix: deployment

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({baseURL: "http://localhost:8000/api", withCredentials: true})
+export const axiosInstance = axios.create({baseURL: "https://curve-9qnx.onrender.com/api", withCredentials: true})
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://curve-nine.vercel.app",
     credentials: true
 }))
 
