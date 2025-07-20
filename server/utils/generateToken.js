d201db18bab3bf02a5a1dc1d1c43d8772d6c1f3a
mohan-bee <mohn08052006@gmail.com>
2025-07-20 13:52:04 +0530

    chore

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,8 +4,8 @@ const generateToken = async (res,user) =>{
        try {
          const token = jwt.sign({id: user._id, username: user.username, email: user.email}, process.env.JWT_SECRET, {expiresIn: "7d"})
         res.cookie("token", token, {
-            httpOnly: true,   // prevents JS access
-            secure: process.env.NODE_ENV === "production", // HTTPS only in prod
+            httpOnly: false,   // prevents JS access
+            secure: true, // HTTPS only in prod
             sameSite: "strict", // or "lax"
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
         })
