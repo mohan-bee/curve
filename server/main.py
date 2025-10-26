acb6ccd8192d78caa40eca0af4cfa2ce400362d3
Mohan <mohan@Mohans-MacBook-Air.local>
2025-10-26 13:24:14 +0530

    removed server only client

--- server/main.py
+++
@@ -1,5 +0,0 @@
-def solution(A):
-    # return your solution here ...
-    return A
-
-print(solution([1,3,5,4,7]))
--- server/routes/coderunner.js
+++
@@ -1,7 +0,0 @@
-const { coderunner } = require('../controllers/coderunner')
-
-const router = require('express').Router()
-
-router.post('/run', coderunner)
-
-module.exports = router
