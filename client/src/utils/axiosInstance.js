ff5259d6a2e1e2796a66914dee9fe0a3025965ed
Mohan <mohan@Mohans-MacBook-Air.local>
2025-10-27 15:22:21 +0530

    fix

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.MODE == "development" ? "http://localhost:8000/api" : "http://16.170.168.114:8080/api"
+const baseURL = import.meta.env.MODE == "development" ? "http://localhost:8000/api" : "http://16.16.76.79:8080/api"
 console.log(import.meta.env)
 console.log(baseURL)
 export const axiosInstance = axios.create({baseURL, withCredentials: true})
