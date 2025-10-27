4a13f727f967a3a6b5c3a225ecce40247230022e
Mohan <mohan@Mohans-MacBook-Air.local>
2025-10-27 13:37:23 +0530

    yml fix

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,14 +8,15 @@ const generateToken = async (res, user) => {
             { expiresIn: "7d" }
         );
         
-        // Properly configure cookie for cross-origin requests
+       
         res.cookie("token", token, {
-            maxAge: 7 * 24 * 60 * 1000, // 7 days
-            httpOnly: true,                   // Prevent XSS attacks
-            secure: process.env.ENV !== "development", // Use secure in production
-            sameSite: process.env.ENV === "development" ? "lax" : "none", // Allow cross-origin in production
+            maxAge: 7 * 24 * 60 * 1000, 
+            httpOnly: true,                  
+       //      secure: process.env.ENV !== "development",
+            secure :false,
+            sameSite: process.env.ENV === "development" ? "lax" : "none", 
         });
-
+        console.log(res.cookies, token)
         return token
     } catch (error) {
         console.log("Error generating Token: ", error.message)
