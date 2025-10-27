b69ccc4a855148c13371f084ecde8ae0cff3c6bf
Mohan <mohan@Mohans-MacBook-Air.local>
2025-10-27 13:48:10 +0530

    yml fix

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -14,40 +14,12 @@ const generateToken = async (res, user) => {
         console.log(`[Token Generation] Generated Token: ${token.substring(0, 10)}...`); // Log a snippet
         // ----------------------
        
-        // Recommended Cookie Options Adjustment
-        const cookieOptions = {
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
-            httpOnly: true,                  
-            // Since you are running on http://16.170.168.114, 'secure: true' will prevent the cookie from being set.
-            // Temporarily set 'secure: false' for HTTP testing, but use 'true' in production with HTTPS.
-            secure: false, // Must be false for HTTP testing
-
-            // When frontend (5173) and backend (8080) are different ports (cross-origin), 
-            // you need 'sameSite: "none"' for the browser to send it, BUT 'secure: true' is REQUIRED 
-            // by browsers (Chrome, Firefox, etc.) if 'sameSite: "none"' is used.
-            // Since you are using 'secure: false', you might need to temporarily use 'lax' or 'strict'
-            // or simply remove 'sameSite' entirely if you're not using "none".
-            // However, since it is a cross-origin scenario (different ports), "none" is usually required.
-
-            // Let's use 'none' and keep 'secure: false' for debugging, but be aware this combination might fail
-            // in modern browsers. The most reliable fix is to use HTTPS and set 'secure: true'.
-            sameSite: "none", 
-        };
-
-        // If 'sameSite: "none"' and 'secure: false' does not work, try this combination:
-        // const cookieOptions = {
-        //     maxAge: 7 * 24 * 60 * 60 * 1000,
-        //     httpOnly: true,
-        //     secure: false,
-        //     sameSite: "lax", // This works for HTTP on the *same domain* (which IP is) but might fail on different ports
-        // };
-
-        res.cookie("token", token, cookieOptions);
-
-        console.log("[Cookie Setting] Cookie set with options:", cookieOptions);
-        // Note: res.cookies is not a standard way to see the set cookie object in Express response.
-        // The cookie is set in the 'Set-Cookie' header, which you can inspect in your browser's Network tab.
-        // You should see the 'Set-Cookie' header in the backend's response.
+       res.cookie("token", token, {
+              maxAge: 7 * 24 * 60 * 60 * 1000, 
+              httpOnly: true,                  
+              secure: false,  // KEEP THIS AS FALSE (because you're on HTTP)
+              sameSite: "lax", // 💡 CHANGE THIS TO 'lax'
+              });
         
         return token
     } catch (error) {
