ecaa8da89883959c34e835a639b1f24f16c0f05a
Mohan <mohan@Mohans-MacBook-Air.local>
2025-10-27 09:57:57 +0530

    added cicd for client

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const baseURL = import.meta.env.MODE == "development" ? "http://localhost:8000/api" : "https://curve-9qnx.onrender.com/api"
-const baseURL = "http://localhost:8000/api"
-
+const baseURL = import.meta.env.MODE == "development" ? "http://localhost:8000/api" : "http://16.170.168.114:8080/api"
+console.log(import.meta.env)
 console.log(baseURL)
 export const axiosInstance = axios.create({baseURL, withCredentials: true})
